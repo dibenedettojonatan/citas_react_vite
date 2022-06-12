52c8c62b1cb8418bd5986a4e1e14ed0ab690a1a3
dibenedetto <dibenedettojonatan@hotmail.com>
2022-06-12 14:48:07 +0200

    Primer Commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+const { postcss } = require('autoprefixer')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    'index.html',
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
